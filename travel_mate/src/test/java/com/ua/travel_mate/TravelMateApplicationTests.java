a40ae842d06c414ad2283f1d3d3c44798a12663b
Oryna Kasapova <oryna.kasapova@nure.ua>
2024-08-07 17:43:03 +0200

    JUnit with database

--- travel_mate/src/test/java/com/ua/travel_mate/TravelMateApplicationTests.java
+++ travel_mate/src/test/java/com/ua/travel_mate/TravelMateApplicationTests.java
@@ -1,21 +1,44 @@
 package com.ua.travel_mate;
 
 import com.ua.travel_mate.controllers.UserController;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 class TravelMateApplicationTests {
 
-	@Autowired
-	private UserController controller;
+    @Autowired
+    private UserController controller;
 
-	@Test
-	void contextLoads() throws Exception{
-		assertThat(controller).isNotNull();
-	}
+    static final MySQLContainer MY_SQL_CONTAINER;
+
+    static {
+        MY_SQL_CONTAINER = new MySQLContainer("mysql:8");
+        MY_SQL_CONTAINER.start();
+    }
+
+    @DynamicPropertySource
+    static void configureTestProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () -> MY_SQL_CONTAINER.getJdbcUrl());
+        registry.add("spring.datasource.username", () -> MY_SQL_CONTAINER.getUsername());
+        registry.add("spring.datasource.password", () -> MY_SQL_CONTAINER.getPassword());
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+
+    }
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
+    }
 
 }
